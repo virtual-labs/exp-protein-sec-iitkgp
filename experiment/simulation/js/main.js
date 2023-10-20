98b44ca7f25780c4aca4e6febcc7fb0a2874bc7f
prakriti5dhang <prakritidhang@gmail.com>
2023-10-20 12:24:37 +0530

    exp7

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,4 @@
-//Your JavaScript goes in here
+/* Lab name: Experimental Biochemistry
+Exp name: Protein purification by size exclusion chromatography (SEC)
+File name: main.js
+Developer: Prakriti Dhang */
