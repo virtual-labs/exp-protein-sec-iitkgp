8d1eccd16658d918e435405cf01c27d7df9d9658
prakriti5dhang <prakritidhang@gmail.com>
2023-10-22 11:08:23 +0530

    exp7

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -2,3 +2,35 @@
 Exp name: Protein purification by size exclusion chromatography (SEC)
 File name: main.js
 Developer: Prakriti Dhang */
+
+
+function start(){
+    document.getElementById("step1").disabled=false; 
+}
+
+function proteinsample(){
+
+    document.getElementById("falcon").setAttribute("onclick", "zoomprotein()");
+    document.getElementById("step1").disabled=true;
+}
+
+function zoomprotein(){
+
+}
+
+function injectbuffer(){
+
+}
+
+
+function injectps(){
+
+}
+
+function elution(){
+
+}
+
+function dataplot(){
+    
+}
